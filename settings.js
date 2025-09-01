dac7a3ddd3d472cfa3b3638e44e0a831a5cbef87
Olimar <gpfernandezflorio@gmail.com>
2025-09-01 16:18:46 -0300

    Cambio la url del servidor

--- settings.js
+++ settings.js
@@ -3,7 +3,8 @@
 // window.actividad_obligatoria = { curso:'inpr_unq_2023_s1', actividad:'Rosa de los vientos' }; // el curso y el ejercicio
 
 // URL del servidor
-const urlServidor = "https://epli.exp.dc.uba.ar"
+// const urlServidor = "https://epli.exp.dc.uba.ar"
+const urlServidor = "http://157.92.26.79:8060"
 // ¿Está habilitado el servidor de redirección?
 const servidorDeRedireccion = false;
 const puertoInicial = 8040; // En caso de usar el servidor de redirección
